0dbddaecc0fc7fbff287cf8b22f7f9ccb040ace5
tdnshah <sankettejas@gmail.com>
2023-09-15 17:44:52 +0530

    Enabled to the project, Remove @astro/2.5.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import alpinejs from "@astrojs/alpinejs";
 
-// https://astro.build/config
 import compress from "astro-compress";
 
 // https://astro.build/config
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
-import image from "@astrojs/image";
+
+// https://astro.build/config
+import decapCmsOauth from "astro-decap-cms-oauth";
+
+// https://astro.build/config
+import node from "@astrojs/node";
 
 // https://astro.build/config
 
@@ -30,15 +29,17 @@ export default defineConfig({
     config: {
       applyBaseStyles: false
     }
-  }), sitemap(), mdx(), alpinejs(), compress(), prefetch(), image({
-    logLevel: 'debug'
-  })],
+  }), sitemap(), mdx(), alpinejs(), compress(), prefetch(), decapCmsOauth()],
   vite: {
     build: {
       exclude: "config.yml",
       rollupOptions: {
-        external: "config.yml",
-      },
-    },
+        external: "config.yml"
+      }
+    }
   },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
